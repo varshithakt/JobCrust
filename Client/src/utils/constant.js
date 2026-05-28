e5b94cd19ae31969b29d4f6692d287582779ab2f
Varshitha K T <varshithakt712@gmail.com>
2026-05-28 13:58:54 +0530

    fixed production backend urls

--- Client/src/utils/constant.js
+++ Client/src/utils/constant.js
@@ -1,8 +1,12 @@
-const backend_url = import.meta.env.VITE_BACKEND_URL
-export const USER_API_END_POINT = `${backend_url}/api/v1/user`
-export const JOB_API_END_POINT = `${backend_url}/api/v1/job`
-export const APPLICANT_API_END_POINT = `${backend_url}/api/v1/application`
-export const COMPANY_API_END_POINT = `${backend_url}/api/v1/company`
+//const backend_url = import.meta.env.VITE_BACKEND_URL
+//export const USER_API_END_POINT = `${backend_url}/api/v1/user`
+//export const JOB_API_END_POINT = `${backend_url}/api/v1/job`
+//export const APPLICANT_API_END_POINT = `${backend_url}/api/v1/application`
+//export const COMPANY_API_END_POINT = `${backend_url}/api/v1/company`
+export const USER_API_END_POINT = "https://jobcrust.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobcrust.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobcrust.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobcrust.onrender.com/api/v1/company";
 
 // export const USER_API_END_POINT = "http://localhost:8000/api/v1/user"
 // export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job"
